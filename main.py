2f210e387c89105cf8b48a691e08b74d65b5b397
anatoly <anatoly.ripak@gmail.com>
2024-07-04 16:51:15 +0300

    Сделано нахождение координат щита и отображение в круговых координатах для произвольного количества щитов и произвольного азимута на Юг+Плоский

--- main.py
+++ main.py
@@ -8,9 +8,23 @@ import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import plotly.express as px
 
+p = 263.6
+centers_distance = 156.2  # расстояние между геометрическим центром телескопа и фокусом антенны
+angle_per_panel = 24 / 60 * np.pi / 180
+n_left = 100
+n_right = 233
+pi = np.pi
 
-def convert_polar_to_polar(_radius, _angle, _centers_distance):
-    _dR = _centers_distance
+
+def convert_polar_to_polar(_radius, _angle):
+    """
+    Функция возвращает положение щита в полярной системе, связанной с геометрическим центром телескопа по известному
+    положению щита (_radius и _angle)в полярной системе, связанной с фокусом антенны (_radius_out, _angle_out)
+    :param _radius:
+    :param _angle:
+    :return: положение щита в полярной системе, связанной с фокусом антенны
+    """
+    _dR = centers_distance
     _radius_out = np.sqrt(_radius ** 2 + _dR ** 2 - 2 * _radius * _dR * np.cos(_angle))
     _coeff = _radius / _radius_out
     _angle_out = np.pi - np.arcsin(np.sin(_angle) * _coeff)
@@ -18,19 +32,32 @@ def convert_polar_to_polar(_radius, _angle, _centers_distance):
 
 
 def func_under_solve(_angle1):
-    _angle2 = np.linspace(-0.151, 0.151, 137)
-    _angle2 = (_angle2 + 1) * np.pi
-    _p = 263.6
-    _dr = 156.2
-    _r1 = _p / (1 - np.cos(_angle1))
-    _r2 = np.sqrt(_r1 ** 2 + _dr ** 2 - 2 * _r1 * _dr * np.cos(_angle1))
+    """
+    Целевая функция для нахождения углового положения центра щита относительно фокуса параболы _angle1 при его заданной
+    угловой координате относильно геометрического центра радиотелескопа _angle2
+    :param _angle1: угол относительно фокуса параболы
+    :return: угол относильно геометрического центра радиотелескопа
+    """
+    _angle2 = panel_angles()
+    dr = centers_distance
+    _r1 = p / (1 - np.cos(_angle1))
+    _r2 = np.sqrt(_r1 ** 2 + dr ** 2 - 2 * _r1 * dr * np.cos(_angle1))
     _coeff = _r1 / _r2
     _f = np.pi - np.arcsin(np.sin(_angle1) * _coeff) - _angle2
     return _f
 
 
-def plot_polar(_radius, _angle):
+def panel_angles():
+    _n_sector = n_right - n_left + 1
+    _sector_left_pos = (150 - n_left + 1 / 2) * angle_per_panel
+    _sector_right_pos = (n_right - 150 + 1 / 2) * angle_per_panel
+    _panel_angle_pos = np.linspace((_sector_left_pos - angle_per_panel / 2),
+                                   -(_sector_right_pos - angle_per_panel / 2),
+                                   _n_sector) + np.pi
+    return _panel_angle_pos
 
+
+def plot_polar(_radius, _angle):
     r = _radius
     theta = _angle
     plt.polar(theta, r)
@@ -38,40 +65,49 @@ def plot_polar(_radius, _angle):
     plt.show()
 
 
-def plotly_polar(_theta, _radial):
+def plotly_polar(_theta, _radial, _plotly_dict):
     _theta = _theta / 2 / np.pi * 360
     fig = px.scatter_polar(
-            r=_radial,
-            theta=_theta,
-            start_angle=0,
-            direction="counterclockwise",
-            range_theta=[150, 210]
-        )
+        r=_radial,
+        theta=_theta,
+        start_angle=0,
+        direction="counterclockwise",
+        range_theta=_plotly_dict['range_theta']
+    )
+
+    fig.update_layout(
+        polar=_plotly_dict['polar']
+    )
+
     fig.write_html('pic_sign', auto_open=True)
-    fig.show()
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    centers_distance = 156.2
-    p = 263.6
-    pi = np.pi
-    angle0 = pi / 8
+    angle0d = -28  # Азимут в градусах
+    angle0 = angle0d / 180 * pi
     angle = np.linspace(pi * (1 - 0.25), pi * (1 + 0.25), 100)
-    r1 = 263.6 / (1 - np.cos(angle))
-    # angle = angle - pi / 8
+    r1 = p / (1 - np.cos(angle))
+
     # plot_polar(r1, angle)
-    r2, angle2 = convert_polar_to_polar(r1, angle, centers_distance)
+    r2, angle2 = convert_polar_to_polar(r1, angle)
     # plot_polar(r2, angle2 - pi / 8)
 
-    z = scipy.optimize.root(func_under_solve, x0=[pi] * 137)
+    z = scipy.optimize.root(func_under_solve, x0=[pi] * (n_right - n_left + 1))
     parabola_arg = z.x
-    parabola_ord = p / (1 - np.cos(parabola_arg))
-    radius_out = np.sqrt(parabola_ord ** 2 + centers_distance ** 2 -
-                         2 * parabola_ord * centers_distance * np.cos(parabola_arg))
+    parabola_value = p / (1 - np.cos(parabola_arg))
+    radius_out = np.sqrt(parabola_value ** 2 + centers_distance ** 2 -
+                         2 * parabola_value * centers_distance * np.cos(parabola_arg))
 
-    # print(z.x)
-    plotly_polar(np.linspace(-0.151, 0.151, 137) * pi + pi, radius_out)
+    plotly_dict = {'range_theta': [140 - angle0d, 220 - angle0d],
+                   'polar': {
+                       "angularaxis": {
+                           "tickmode": "array",
+                           "tickvals": list(range(0, 360, 360 // 90)),
+                           "ticktext": [f"{a - 300}" for a in range(900, 0, -900 // 90)],
+                                       }
+                             }
+                   }
+    plotly_polar(panel_angles() - angle0, radius_out, plotly_dict)
 
     # plt.polar( (np.linspace(-0.151, 0.151, 137) * pi + pi), radius_out)
     # plt.set_ylim(200, 300)
